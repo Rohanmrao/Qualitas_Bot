dd0ba83afab3dc95a7becc4ce60a064572710298
Rohanmrao <rohanmaheshrao10@gmail.com>
2023-10-27 17:11:33 +0530

    single bash script for startup

--- server.bash
+++ server.bash
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
 
-# Open first terminal and enter directory
 gnome-terminal --working-directory=home/rohan/qualitas_bot/UI/qualitasbot --tab --title="UI Server" --command="serve -s build"
-
-# Open second terminal and enter directory
 gnome-terminal --working-directory=home/rohan/qualitas_bot/chatbot --tab --title="Chatbot Server" --command="flask run --host=0.0.0.0"
